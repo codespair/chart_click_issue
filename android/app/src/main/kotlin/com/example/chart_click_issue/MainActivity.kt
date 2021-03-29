a34b30e6f5ed008a155e36edf400c3c613db957f
codespair <codespairams@gmail.com>
2021-03-29 22:06:58 +0200

    initial working sample with chart and list

---
+++ android/app/src/main/kotlin/com/example/chart_click_issue/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chart_click_issue
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,207 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'dart:math';
+
+void main() {
+  runApp(ProviderScope(child: MyApp()));
+}
+
+// riverpod provider
+final carProvider = FutureProvider((_) => _allCarSales());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends HookWidget {
+  final double itemExtentSize = 70.0;
+  @override
+  Widget build(BuildContext context) {
+    final _carProvider = useProvider(carProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Car Sales'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            color: Colors.white,
+            padding: EdgeInsets.fromLTRB(20, 20, 15, 5),
+            height: 200,
+            child: _carProvider.when(
+              data: (carList) => _buildLineChart(carList),
+              loading: () => CircularProgressIndicator(),
+              error: (_, __) => Text('Ooooopsss error'),
+            ),
+          ),
+          Expanded(
+            child: _carProvider.when(
+              data: (carList) => _getSlidableCarList(carList, context),
+              loading: () => CircularProgressIndicator(),
+              error: (_, __) => Text('Ooopsss error'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildLineChart(List<CarSales> carList) {
+    var bpChartData = _BPChartData.build(carList);
+    return SizedBox(
+      key: UniqueKey(),
+      width: 350,
+      height: 200,
+      child: LineChart(
+        LineChartData(
+          backgroundColor: Colors.white,
+          lineTouchData: LineTouchData(
+            enabled: true,
+          ),
+          lineBarsData: [
+            LineChartBarData(
+              spots: bpChartData.carPoints,
+              isCurved: true,
+              barWidth: 1.5,
+              colors: [
+                Colors.deepPurple,
+              ],
+              dotData: FlDotData(
+                show: false,
+              ),
+            ),
+          ],
+          minY: bpChartData.minY.toDouble() - 100,
+          maxY: bpChartData.maxY.toDouble() + 100,
+          titlesData: FlTitlesData(
+            bottomTitles: SideTitles(
+                showTitles: true,
+                interval: bpChartData.labelXInterval.toDouble(),
+                rotateAngle: -15,
+                getTextStyles: (value) =>
+                    const TextStyle(fontSize: 10, color: Colors.deepPurple),
+                getTitles: (value) {
+                  return bpChartData.models[value.toInt()];
+                }),
+            leftTitles: SideTitles(
+              showTitles: true,
+              interval: bpChartData.labelYInterval.toDouble(),
+              getTitles: (value) {
+                return '$value';
+              },
+            ),
+          ),
+          gridData: FlGridData(
+            show: false,
+            horizontalInterval: 2,
+          ),
+          borderData: FlBorderData(
+            show: true,
+            border: const Border(
+              left: BorderSide(color: Colors.deepPurple),
+              top: BorderSide(color: Colors.transparent),
+              bottom: BorderSide(color: Colors.deepPurple),
+              right: BorderSide(color: Colors.transparent),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _getSlidableCarList(List<CarSales> carList, BuildContext context) {
+    return ListView.builder(
+      itemExtent: itemExtentSize,
+      itemCount: carList.length,
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        return Slidable(
+            key: UniqueKey(),
+            closeOnScroll: true,
+            child: ListTile(
+              title: Text('${carList[index].car}',
+                  style: Theme.of(context).textTheme.headline5),
+              subtitle: Text(
+                '${carList[index].brand}',
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
+              trailing: Text('${carList[index].units} units'),
+            ));
+      },
+    );
+  }
+}
+
+class _BPChartData {
+  List<FlSpot> carPoints;
+  int maxY;
+  int minY;
+  List<String> models;
+  int labelXInterval;
+  int labelYInterval;
+
+  _BPChartData.build(List<CarSales> carList) {
+    carList = carList.reversed.toList(growable: false);
+    carPoints = <FlSpot>[];
+    models = <String>[];
+    maxY = carList.map((c) => c.units).reduce(max).ceil();
+    minY = carList.map((c) => c.units).reduce(min).floor();
+    carList.asMap().forEach((index, car) {
+      carPoints.add(FlSpot(index.toDouble(), car.units.toDouble()));
+      models.add(car.car);
+    });
+    labelXInterval = 3;
+    labelYInterval = 100;
+  }
+}
+
+class CarSales {
+  String brand;
+  String car;
+  int units;
+  CarSales(this.brand, this.car, this.units);
+}
+
+Future<List<CarSales>> _allCarSales() async {
+  var result = <CarSales>[];
+  result
+    ..add(CarSales('VW', 'Beetle', 251))
+    ..add(CarSales('VW', 'Golf', 334))
+    ..add(CarSales('VW', 'Passat', 121))
+    ..add(CarSales('VW', 'Polo', 671))
+    ..add(CarSales('VW', 'Tiguan', 251))
+    ..add(CarSales('BMW', 'M5', 586))
+    ..add(CarSales('BMW', 'Z3', 224))
+    ..add(CarSales('BMW', 'Z4', 332))
+    ..add(CarSales('BMW', 'M5', 524))
+    ..add(CarSales('BMW', 'X1', 145))
+    ..add(CarSales('Audi', 'Q3', 754))
+    ..add(CarSales('Audi', 'A3', 864))
+    ..add(CarSales('Audi', 'RS7', 254))
+    ..add(CarSales('Mercedes', 'Maybach', 325))
+    ..add(CarSales('Mercedes', 'S-Class', 755))
+    ..add(CarSales('Mercedes', 'E-Class', 256))
+    ..add(CarSales('Mercedes', 'A-Class Limo', 478))
+    ..add(CarSales('Toyota', 'Aigo', 885))
+    ..add(CarSales('Toyota', 'Corolla', 285))
+    ..add(CarSales('Toyota', 'Yaris', 125))
+    ..add(CarSales('Toyota', 'Prius', 814))
+    ..add(CarSales('Tesla', 'Model X', 754))
+    ..add(CarSales('Tesla', 'Model S', 356));
+
+  return result;
+}
